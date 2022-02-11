ece9058f83708b3034c3539a55d0cc4b7c48dae6
inx-tapan <tapan.inx@gmail.com>
2022-02-11 18:21:27 +0530

    Built the NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routess from "./Routess";
+import Navbar from "./sharedComponents/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="App">
         <Routess />
       </div>
