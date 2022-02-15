0d589ed816242f6d4c201fab96975ecc8901f0f2
inx-tapan <tapan.inx@gmail.com>
2022-02-15 17:26:26 +0530

    Update Footer desktop design

--- src/static/images.js
+++ src/static/images.js
@@ -1,6 +1,18 @@
 import Logo from "../assets/images/logo.svg";
+import spritacularWhite from "../assets/images/Spritacular-white.png";
+import Nasa from "../assets/images/nasa.png";
+import Catholic from "../assets/images/catholic.png";
+import Facebook from "../assets/images/facebook.png";
+import Linkedin from "../assets/images/linkedin.png";
+import Twitter from "../assets/images/twitter.png";
 
 const Images = {
   Logo,
+  spritacularWhite,
+  Nasa,
+  Catholic,
+  Facebook,
+  Linkedin,
+  Twitter
 };
 export default Images;
