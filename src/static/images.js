a35fb6d0b53617fd8e457c5f8d4ee051f73aba53
inx-tapan <tapan.inx@gmail.com>
2022-02-16 12:22:25 +0530

    "set login sign-up modal design."

--- src/static/images.js
+++ src/static/images.js
@@ -9,6 +9,17 @@ import Catholic from "../assets/images/catholic.png";
 import Facebook from "../assets/images/facebook.png";
 import Linkedin from "../assets/images/linkedin.png";
 import Twitter from "../assets/images/twitter.png";
+import Register from "../assets/images/Register.png";
+import Dragdrop from "../assets/images/drag-drop.jpg";
+import Jet from "../assets/images/jet.jpg";
+import HowitworksSecond from "../assets/images/Howitworks_second.png";
+import Topimage from "../assets/images/how-it-works-top.png";
+import Registerpolygon from "../assets/images/register-polygon.png";
+import Observepolygon from "../assets/images/Learn-observe-polygon.png";
+import Sharepolygon from "../assets/images/share-polygon.png";
+import Engagepolygon from "../assets/images/engage-polygon.png";
+import Bottompolygon from "../assets/images/bottom-polygon.png";
+import Modalcloseicon from "../assets/images/close-icon.svg";
 
 const Images = {
   Logo,
@@ -22,5 +33,17 @@ const Images = {
   Facebook,
   Linkedin,
   Twitter,
+  Register,
+  Dragdrop,
+  Jet,
+  HowitworksSecond,
+  Topimage,
+  Registerpolygon,
+  Observepolygon,
+  Sharepolygon,
+  Engagepolygon,
+  Engagepolygon,
+  Bottompolygon,
+  Modalcloseicon,
 };
 export default Images;
