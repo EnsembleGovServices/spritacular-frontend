1d0600976ba0650c8e42b7677064012a74bb407f
inx-tapan <tapan.inx@gmail.com>
2022-02-18 16:08:55 +0530

    "set user menu dropdown after login."

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,32 +1,36 @@
 import {
   Button,
   Collapse,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
   Nav,
   Navbar,
   NavbarToggler,
-  NavItem
+  NavItem,
 } from "reactstrap";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Images from "../../static/images";
 import LoginPopup from "../popup/LoginPopup";
 import RegisterPopup from "../popup/RegisterPopup";
 import routesList from "../../routes/MainRoute";
-import '../../assets/scss/component/header.scss';
-import {Link, useLocation} from "react-router-dom";
+import "../../assets/scss/component/header.scss";
+import { Link, useLocation } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 const Header = (props) => {
   const { auth } = useAuth();
   const [isLoginModal, setIsLoginModal] = useState(false);
   const [isRegisterModal, setIsRegisterModal] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const location = useLocation();
-  const homeUrl = location.pathname === '/';
+  const homeUrl = location.pathname === "/";
 
   useEffect(() => {
-
     document.addEventListener("scroll", () => {
       let scroll = window.pageYOffset || document.documentElement.scrollTop,
-          navbarEl = document.querySelector(".custome-header");
+        navbarEl = document.querySelector(".custome-header");
 
       if (scroll > 80) {
         navbarEl.classList.add("bg-color-menu");
@@ -47,46 +51,60 @@ const Header = (props) => {
     setShowMenu(!showMenu);
   };
 
+  const handleUserMenuDropdown = () => {
+    setShowUserMenu(!showUserMenu);
+  };
 
   return (
-      <>
-        <div>
-          <Navbar container expand="md" className={homeUrl ? 'custome-header' : 'custome-header bg-not-home'} light>
-            <Link to="/" className="navbar-brand">
-              <img src={Images.Logo} alt="Logo" className="logo" />
-              <img src={Images.BlackLogo} alt="Logo" className="on-scroll-logo" />
-            </Link>
-            <NavbarToggler onClick={() => menuToggle()} />
-            <Collapse navbar isOpen={showMenu}>
-              <Nav className="" navbar>
-                {
-                  routesList.filter((item) => item.name !== 'home').map((route, index) => {
-                    return (
-                        <NavItem key={index}>
-                          <Link to={route.path} title={route.name} className="nav-link text-capitalize">{route.name}</Link>
-                        </NavItem>
-                    )
-                  })
-                }
-                <NavItem className="d-md-none d-xs-block">
-                  <Button
-                      className="register nav-link"
-                      onClick={() => handleRegisterModal()}
-                  >
-                    Register
-                  </Button>
-                </NavItem>
-                <NavItem className="d-md-none d-xs-block">
-                  <Button
-                      className="nav-link login-btn"
-                      onClick={() => handleLoginModal()}
-                  >
-                    Login
-                  </Button>
-                </NavItem>
-              </Nav>
-            </Collapse>
-            {!auth ? (
+    <>
+      <div>
+        <Navbar
+          container
+          expand="md"
+          className={homeUrl ? "custome-header" : "custome-header bg-not-home"}
+          light
+        >
+          <Link to="/" className="navbar-brand">
+            <img src={Images.Logo} alt="Logo" className="logo" />
+            <img src={Images.BlackLogo} alt="Logo" className="on-scroll-logo" />
+          </Link>
+          <NavbarToggler onClick={() => menuToggle()} />
+          <Collapse navbar isOpen={showMenu}>
+            <Nav className="" navbar>
+              {routesList
+                .filter((item) => item.name !== "home")
+                .map((route, index) => {
+                  return (
+                    <NavItem key={index}>
+                      <Link
+                        to={route.path}
+                        title={route.name}
+                        className="nav-link text-capitalize"
+                      >
+                        {route.name}
+                      </Link>
+                    </NavItem>
+                  );
+                })}
+              <NavItem className="d-md-none d-xs-block">
+                <Button
+                  className="register nav-link"
+                  onClick={() => handleRegisterModal()}
+                >
+                  Register
+                </Button>
+              </NavItem>
+              <NavItem className="d-md-none d-xs-block">
+                <Button
+                  className="nav-link login-btn"
+                  onClick={() => handleLoginModal()}
+                >
+                  Login
+                </Button>
+              </NavItem>
+            </Nav>
+          </Collapse>
+          {/* {!auth ? (
                 <div className="right-menu">
                   <Button
                       className="register nav-link"
@@ -107,21 +125,41 @@ const Header = (props) => {
                 >
                   Hi, {auth?.first_name}
                 </h5>
-            )}
-          </Navbar>
-        </div>
+            )} */}
+          <div className="after-login-right-menu">
+            <Dropdown
+              className="user-menu"
+              isOpen={showUserMenu}
+              toggle={handleUserMenuDropdown}
+            >
+              <DropdownToggle caret>
+                <img src={Images.UserPlaceholder} alt="UserPlaceholder" /> Carl
+                Nielsen
+              </DropdownToggle>
+              <DropdownMenu container="body">
+                <DropdownItem>Edit Profile</DropdownItem>
+                <DropdownItem>Change Password</DropdownItem>
+                <DropdownItem>Logout</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </Navbar>
+      </div>
 
-        {isLoginModal && (
-            <LoginPopup open={auth ? false: isLoginModal} handleClose={handleLoginModal} />
-        )}
+      {isLoginModal && (
+        <LoginPopup
+          open={auth ? false : isLoginModal}
+          handleClose={handleLoginModal}
+        />
+      )}
 
-        {isRegisterModal && (
-            <RegisterPopup
-                open={auth ? false: isRegisterModal}
-                handleClose={handleRegisterModal}
-            />
-        )}
-      </>
+      {isRegisterModal && (
+        <RegisterPopup
+          open={auth ? false : isRegisterModal}
+          handleClose={handleRegisterModal}
+        />
+      )}
+    </>
   );
 };
 export default Header;
