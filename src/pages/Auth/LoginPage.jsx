4d1fdc38dd3bd8d8412a8005d837e841ceb22781
inx-tapan <tapan.inx@gmail.com>
2022-03-01 14:01:01 +0530

    "Set login page design, Set upload observation menu sticky."

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -1,25 +1,28 @@
 import Login from "../../components/Auth/Login";
-import {Col, Container, Row} from "reactstrap";
+import { Container } from "reactstrap";
 import useAuth from "../../hooks/useAuth";
-import {Navigate, useLocation} from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
+import "../../assets/scss/component/modal.scss";
 
 const LoginPage = () => {
-    const {auth, persist} = useAuth();
-    const location = useLocation();
+  const { auth, persist } = useAuth();
+  const location = useLocation();
 
-    if (auth && persist) {
-        return <Navigate to="/" state={{ from: location }} replace />
-    }
-    return(
-        <section className="min-vh-100 d-flex align-items-center justify-content-center">
-            <Container>
-                <Row>
-                    <Col sm={{size:4, offset: 4}}>
-                        <Login />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
-}
+  if (auth && persist) {
+    return <Navigate to="/" state={{ from: location }} replace />;
+  }
+  return (
+    <section className="min-vh-100 d-flex align-items-center justify-content-center">
+      <Container>
+        <div className="common-modal modal-content-page">
+          <div className="modal-content ">
+            <div className="modal-body">
+              <Login />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
 export default LoginPage;
