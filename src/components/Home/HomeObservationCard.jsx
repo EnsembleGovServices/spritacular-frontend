1bf60b5f9aff20dd53f97b46806bf543d0c27b95
inx-tapan <tapan.inx@gmail.com>
2022-03-04 13:39:10 +0530

    Update home page card image

--- src/components/Home/HomeObservationCard.jsx
+++ src/components/Home/HomeObservationCard.jsx
@@ -1,53 +1,58 @@
 import { Row, Col } from 'reactstrap';
 import ObservationCard from '../Shared/ObservationCard';
+import Images from '../../static/images';
 
 const HomeObservationCard = () => {
     const observationCards=[
         {
             img: "image",
-            imgCategory: "imagepath",
+            imgCategory: Images.card1,
             date:"17 June 2022",
             time: "5:23:00",
+            imageFormat: Images.Sprite,
             username:"John Doe",
             userProfile: "imagepath",
             userCountryName: "Trieben, AT",
-            userCountryIcon: "imagepath"
+            userCountryIcon: Images.Flag
         },
         {
             img: "image",
-            imgCategory: "imagepath",
+            imgCategory: Images.card2,
             date:"17 June 2022",
             time: "5:23:00",
-            username:"John Doe",
+            imageFormat: Images.Sprite,
+            username:"Emily White",
             userProfile: "imagepath",
             userCountryName: "Trieben, AT",
-            userCountryIcon: "imagepath"
+            userCountryIcon: Images.Flag
         },
         {
             img: "image",
-            imgCategory: "imagepath",
+            imgCategory: Images.card3,
             date:"17 June 2022",
             time: "5:23:00",
-            username:"John Doe",
+            imageFormat: Images.Bluejet,
+            username:"Jane Ford",
             userProfile: "imagepath",
             userCountryName: "Trieben, AT",
-            userCountryIcon: "imagepath"
+            userCountryIcon: Images.Flag
         },
         {
             img: "image",
-            imgCategory: "imagepath",
+            imgCategory: Images.card4,
             date:"17 June 2022",
             time: "5:23:00",
-            username:"John Doe",
+            imageFormat: Images.GiganticJet,
+            username:"Alex Smith",
             userProfile: "imagepath",
             userCountryName: "Trieben, AT",
-            userCountryIcon: "imagepath"
+            userCountryIcon: Images.Flag
         },
     ]
     return (
         <>
             <Row>
-                {observationCards && observationCards.map((cardItems, index)=>{
+                {observationCards && observationCards?.map((cardItems, index)=>{
                 return (
                     <Col key={index} className="col-6 col-md-3 mb-3 mb-md-0">
                         <ObservationCard cardItems={cardItems} />
