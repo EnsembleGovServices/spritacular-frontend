54893690f49fe07672a608d4c9e8b64354d39489
inx-tapan <tapan.inx@gmail.com>
2022-03-14 12:16:25 +0530

    comment map section

--- src/components/Observation/ObservationLocation.jsx
+++ src/components/Observation/ObservationLocation.jsx
@@ -150,7 +150,7 @@ const ObservationLocation = (props) => {
                             </FormGroup>
                         </Col>}
                     </Row>
-                    <MapWrapper
+                     <MapWrapper
                         google={props.google}
                         center={{ lat: Number((observationImages?.data) ? observationImages?.data[observationImages?.selected_image_index]?.latitude: address1?.markerPosition?.lat), lng: Number((observationImages?.data) ? observationImages?.data[observationImages?.selected_image_index]?.longitude: address1?.markerPosition?.lng) }}
                         height="400px"
@@ -162,7 +162,7 @@ const ObservationLocation = (props) => {
                         mapContainer="map-search-container"
                         searchInputClass="search-input-class"
                         ref={fref}
-                    />
+                    /> 
                 </FormGroup>
             </Col>
             <Col md={12} className="mb-5">
@@ -174,7 +174,7 @@ const ObservationLocation = (props) => {
                             <Col sm={10}>
                                 <Input
                                     // value={address1?.markerPosition?.lat}
-                                    value={(observationImages?.data) ? observationImages?.data[observationImages?.selected_image_index]?.latitude :''}
+                                    value={(observationImages?.data) ? observationImages?.data[observationImages?.selected_image_index]?.latitude : address1?.markerPosition?.lat}
                                     id="LAT"
                                     type="number"
                                     name="latitude"
@@ -190,7 +190,7 @@ const ObservationLocation = (props) => {
                             <Col sm={10}>
                                 <Input
                                     // value={address1?.markerPosition?.lng}
-                                    value={(observationImages?.data) ? observationImages?.data[observationImages?.selected_image_index]?.longitude :''}
+                                    value={(observationImages?.data) ? observationImages?.data[observationImages?.selected_image_index]?.longitude :address1?.markerPosition?.lng}
                                     id="LON"
                                     type="number"
                                     name="longitude"
