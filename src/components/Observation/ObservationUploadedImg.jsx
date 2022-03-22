c894a16cf45bd11ef837cfb8ef40cd84f00a2f1f
inx-tapan <tapan.inx@gmail.com>
2022-03-22 11:51:56 +0530

    Set close icon design for remove image

--- src/components/Observation/ObservationUploadedImg.jsx
+++ src/components/Observation/ObservationUploadedImg.jsx
@@ -1,12 +1,14 @@
 import useObservations from "../../hooks/useObservations";
-import {useEffect, useLayoutEffect, useState} from "react";
+import {useLayoutEffect, useState, useRef} from "react";
 import LazyLoad from "../Upload/LazyLoad";
+import { Icon } from '@iconify/react';
 
 const ObservationUploadedImg = (props) => {
-    const {step, error, obvType}=props;
+    const {obvType, remove, className}=props;
     const {observationImages, setObservationImages} = useObservations();
     const [preview, setPreview] = useState([]);
     const [activeTab, setActiveTab] = useState(observationImages?.selected_image_id ?? null);
+    const imageDelete = useRef(null);
     // Toggle Tabs
     const toggleTab = (tab,index=0) => {
         setActiveTab(tab);
@@ -29,9 +31,14 @@ const ObservationUploadedImg = (props) => {
         <>
             {preview?.map((item, index) => {
                 return(
-                    <div className="d-flex justify-content-end" key={index}>
-                        <button type="button" disabled={obvType?.image_type === 3} className={`position-relative overflow-hidden d-flex p-0 border-0 shadow-none mb-2 selected-image ${activeTab === item?.id ? 'active-tab' : ''}`} onClick={()=> toggleTab(item?.id,index)}>
+                    <div className={className ? `${className} d-flex justify-content-end` : "d-flex justify-content-end"} key={index}>
+                        <button type="button" disabled={obvType?.image_type === 3} className={`position-relative d-flex p-0 shadow-none mb-2 selected-image ${activeTab === item?.id ? 'active-tab' : ''}`} onClick={()=> toggleTab(item?.id,index)}>
                             <LazyLoad src={item?.image} alt={item?.name} />
+                            <button type="button" className="remove-btn text-black border-0 p-0 position-absolute btn" ref={imageDelete} onClick={()=> remove(item?.id)}>
+                                <span>
+                                    <Icon icon="ci:close-big" />
+                                </span>
+                            </button>
                         </button>
                     </div>
                 )
