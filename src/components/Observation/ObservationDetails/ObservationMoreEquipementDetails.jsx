40c5778f15fa8ed063a84f7fead260b942737b92
inx-tapan <tapan.inx@gmail.com>
2022-04-01 11:40:22 +0530

    comments count added to other two tabs

--- src/components/Observation/ObservationDetails/ObservationMoreEquipementDetails.jsx
+++ src/components/Observation/ObservationDetails/ObservationMoreEquipementDetails.jsx
@@ -2,7 +2,7 @@ import { Button, Col, Row } from "reactstrap";
 import { Icon } from '@iconify/react';
 
 const ObservationMoreEquipementDetails = (props) => {
-    const {data} = props;
+    const {data, obvCommentCount} = props;
     return (
         <>
             <Row>
@@ -90,7 +90,7 @@ const ObservationMoreEquipementDetails = (props) => {
                             <div className="d-flex align-items-center justify-content-center user-review">
                                 <span className="me-3 d-flex" ><Icon icon="heroicons-solid:thumb-up" width="17" height="17" className="me-1" /> 2,250 </span>
                                 <span className="me-3 d-flex" ><Icon icon="heroicons-solid:eye" width="17" height="17" className="me-1" /> 100K </span>
-                                <span className="d-flex" ><Icon icon="mdi:message" width="17" height="17" className="me-1" /> 5 </span>
+                                <span className="d-flex" ><Icon icon="mdi:message" width="17" height="17" className="me-1" /> {obvCommentCount} </span>
                             </div>
                         </Col>
                     </Row>
