9a464a79f36d4b7d245c9984e29f54c9bf66c6b8
inx-tapan <tapan.inx@gmail.com>
2022-04-26 17:54:53 +0530

    Notification mark as read updated

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useLayoutEffect} from 'react'
+import React, {useState, useLayoutEffect, useRef} from 'react'
 import {getTokens, onMessageListener} from './firebase';
 import {
   Dropdown,
@@ -11,20 +11,21 @@ import useAuth from "../../hooks/useAuth";
 import Tippy from "@tippyjs/react";
 import moment from "moment";
 import axios from "../../api/axios";
-import {baseURL} from "../..//helpers/url";
+import {baseURL} from "../../helpers/url";
 
 
 
 const Notification = (props) => {
-  const  {notificationArray,setNotificationArray} = props;
-  const {auth} = useAuth();
+    const  {notificationArray,setNotificationArray} = props;
+    const {auth} = useAuth();
     const [show, setShow] = useState(false);
     const [notification, setNotification] = useState(false);
     const [isTokenFound, setTokenFound] = useState(false);
     
     const [notificationDropdown, setNotificationDropdown] = useState(false);
-    // const [notificationArray,setNotificationArray] = useState([]);
     const [data,setData] = useState([]);
+
+    const notificationDropDownRef = useRef(null);
 
     useLayoutEffect(()=> {
       setTokenFound(true);
@@ -34,69 +35,107 @@ const Notification = (props) => {
     }, [isTokenFound])
   
     useLayoutEffect(()=> {
+
       onMessageListener().then(payload => {
-        console.log('payload here', payload);
+        // console.log('payload here', payload);
         setShow(true);
         setNotification(true);
-        setNotificationArray([payload,...notificationArray]);
+        setNotificationArray([...notificationArray, payload]);
         setData([payload,...data]);
       }).catch(err => console.log('failed: ', err));
-    }, [notificationArray, show])
-    const handleNotificationStatusUpdate = (e) => {
-      var notificaitonIds = []
-      data.map((item,index) => {
-        notificaitonIds.push(item.data.notification_id);
-      })
-      axios.post(baseURL.api+'/notification/read_user_notification/',{'notification_ids': notificaitonIds
-       }, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${auth?.token?.access}`
-        }
-    })
-    .then((response)=> {
-      console.log(response);
-      setData([]);
-    })
-    .catch((error)=> {console.log(error)})
+
+    }, [data, notificationArray, setNotificationArray, show])
+
+    const handleNotificationStatusUpdate = async (e, notificaitonIds) => {
+      await axios.post(baseURL.api+'/notification/read_user_notification/',{'notification_ids': notificaitonIds}, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${auth?.token?.access}`
+                }
+            })
+          .then((response)=> {
+                // console.log(response);
+                setData([]);
+                setTimeout(function () {
+                    setNotificationArray([])
+                }, 1500)
+            })
+          .catch((error)=> {
+                console.log(error)
+          })
+    }
+
+    const handleMarkAsRead = (e) => {
+        setNotification(false);
+        const notificaitonIds = []
+        notificationArray.map((item) => {
+            return notificaitonIds.push(item.data.notification_id.toString());
+        })
+        handleNotificationStatusUpdate(e, notificaitonIds).then(r => r)
+
+        let dropContent = document.querySelectorAll('.drp-c');
+        dropContent.forEach(function (item) {
+            return item.classList.add('markedAsRead');
+        })
     }
 
     return (
-            <Dropdown className="notify_menu" onClick={ e => {setNotification(false); if(notification) {handleNotificationStatusUpdate(e)}}} isOpen={notificationDropdown} toggle={ () => setNotificationDropdown(!notificationDropdown)}>
+            <Dropdown className="notify_menu"
+                      isOpen={notificationDropdown}
+                      toggle={ () => setNotificationDropdown(!notificationDropdown)}
+            >
             <DropdownToggle className="notification">
               <Icon icon="ic:baseline-notifications" />
               {notification && <span className="notify" />}
             </DropdownToggle>
-            <DropdownMenu container="body" className="notify-open_menu">
-              <DropdownItem key={0} header> { notificationArray?.length > 0  ? 'Notifications ('+ `${notificationArray?.length}` +')'  : 'No new Notificaitons'} </DropdownItem>
-              <DropdownItem divider />
-              {notificationArray?.map((item,index) => {
-                return (
-                  <div key={index}>
-                  <DropdownItem key={index}>
-                  <div className="notify_wrapper">
-                  <Tippy animation="perspective" content={item.data?.from_user}>
-                    {item.notification?.image  ? 
-                      <i><img src={item.notification?.image} alt="user Profile" /></i> 
-                    : 
-                    <Icon icon="entypo:user" />
-                    }
-                      
-                  </Tippy>
-                    
-                    <div className="comment_wrapper">
-                      <div className="comment_details">
-                        <h4>{item.notification.title}</h4>
-                        <p>{item.notification.body}</p>
-                      </div>
-                      <span>{moment(item.data?.sent_at).fromNow(true)}</span>
+            <DropdownMenu  ref={notificationDropDownRef} container="body" className="notify-open_menu">
+              <DropdownItem key={0} header>
+                { notificationArray?.length > 0  ? (
+                    <div className="d-flex align-items-center justify-content-between">
+                      <span>Notifications {notificationArray?.length}</span>
+                      <button className="btn btn-link p-0 m-0" onClick={event => handleMarkAsRead(event)}>
+                        <Icon icon="bi:check-circle" />
+                      </button>
                     </div>
+                )  : 'No new Notifications'}
+
+              </DropdownItem>
+              <DropdownItem divider />
+              {notificationArray?.length > 0 ? (
+                  notificationArray?.map((item,index) => {
+                      return (
+                          <div key={index} className="drp-c">
+                              <DropdownItem key={index}>
+                                  <div className="notify_wrapper">
+                                      <Tippy animation="perspective" content={item.data?.from_user}>
+                                          {item.notification?.image  ?
+                                              <div className="user-img">
+                                                  <img className="img-fluid" src={item.notification?.image} alt="user Profile" />
+                                              </div>
+                                              :
+                                              <div className="user-img">
+                                                  <Icon className="img-fluid" icon="entypo:user" />
+                                              </div>
+                                          }
+                                      </Tippy>
+                                      <div className="comment_wrapper">
+                                          <div className="comment_details">
+                                              <h4>{item.notification.title}</h4>
+                                              <p>{item.notification.body}</p>
+                                          </div>
+                                          <span>{moment(item.data?.sent_at).fromNow(true)}</span>
+                                      </div>
+                                  </div>
+                              </DropdownItem>
+                              <DropdownItem divider />
+                          </div>
+                      )
+                  })
+              ) : (
+                  <div className="d-flex align-items-center justify-content-center" style={{ height: "190px"}}>
+                      <h5>All caught up!</h5>
                   </div>
-                </DropdownItem>
-                <DropdownItem divider />
-                </div>
-                )
-                })}
+              )}
               </DropdownMenu>
             </Dropdown>
             
