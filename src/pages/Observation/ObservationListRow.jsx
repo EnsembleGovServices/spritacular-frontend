494617fc86c60f824af07f1550307b3ae12d7247
inx-tapan <tapan.inx@gmail.com>
2022-05-04 12:07:43 +0530

    disabled all check input

--- src/pages/Observation/ObservationListRow.jsx
+++ src/pages/Observation/ObservationListRow.jsx
@@ -51,6 +51,7 @@ const ObservationListRow = (props) => {
                            name= {`is_other_${cardItems.id}`}
                            className="me-0 form-check-input"
                            checked={setChecked(cardItems.id)}
+                           disabled={!!allChecked}
                            onChange={(e) => handleChecked(e, cardItems.id)}
                     />
                 </FormGroup>
@@ -60,7 +61,7 @@ const ObservationListRow = (props) => {
                     { cardItems?.image_type === 3 && <div className="multiple-image_icon"><Icon icon="codicon:list-filter" color="black" /></div>}
                     {cardItems?.is_verified && <div className="verify-card"><Icon icon="mdi:check-decagram" color="#27ae60" width="13" height="13" /></div>}
                     { cardItems?.image_type === 3 ?
-                    <CardImageCarousel carouselData={cardItems?.images} />
+                    <CardImageCarousel handleClick={handleClick} carouselData={cardItems?.images} />
                     : <img
                             alt="Card cap"
                             src={(cardData.compressed_image) ? cardData?.compressed_image : cardData?.image}
