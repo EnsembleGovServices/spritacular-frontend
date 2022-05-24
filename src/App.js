008a4fdeb929086acbcddaecd2f0932552a2643d
inx-tapan <tapan.inx@gmail.com>
2022-05-24 18:51:08 +0530

    Roles added

--- src/App.js
+++ src/App.js
@@ -143,7 +143,6 @@ const App = () => {
                             <ResetPasswordPopup/>
                         </Suspense>
                     }/>
-
                     {/*Protected routes*/}
                     <Route element={<RequireAuth allowedRoles={Roles} setAuthValue={authCallBack}/>}>
                         <Route element={
