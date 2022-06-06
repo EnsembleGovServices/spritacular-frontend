508c15089087ee6a62eb50c4e71c9d87319cf8f6
inx-tapan <tapan.inx@gmail.com>
2022-06-06 19:34:33 +0530

    Fix: Loading design in tutorials page

--- src/pages/BlogTutorial/TutorialListCardContent.jsx
+++ src/pages/BlogTutorial/TutorialListCardContent.jsx
@@ -1,17 +1,36 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import BlurImage from "../../components/Common/BlurImage";
+import { useState } from "react";
 
 const TutorialListCardContent = (props) => {
-    const {item} = props;
-    return (
-        <Link to={`${item?.slug ? item?.slug : ''}`} className={`d-block`}>
-            <div className='tutorial-img'>
-                <img src={item?.thumbnail_image} alt={item?.title}/>
-            </div>
-            <h3>
-                {item?.title}
-            </h3>
-            <p>{item?.description}</p>
-        </Link>
-    )
-}
+  const { item } = props;
+
+  const [loaderLoading, setLoaderLoading] = useState(true);
+
+  const handleLoaderLoading = (state) => {
+    setLoaderLoading(state);
+  };
+
+  return (
+    <Link
+      to={`${item?.slug ? item?.slug : ""}`}
+      className={`d-block position-relative`}
+    >
+      <div className="tutorial-img">
+        <BlurImage
+          loaderLoading={handleLoaderLoading}
+          image={item?.thumbnail_image}
+          preview={item?.thumbnail_image}
+          alt={item?.title}
+        />
+      </div>
+      {!loaderLoading && (
+        <>
+          <h3>{item?.title}</h3>
+          <p>{item?.description}</p>
+        </>
+      )}
+    </Link>
+  );
+};
 export default TutorialListCardContent;
