6d0828b6cb5b6becbce6f0f1123b83ef3658d8d4
inx-tapan <tapan.inx@gmail.com>
2022-06-09 19:54:04 +0530

    design update

---
+++ src/helpers/frontPage.js
@@ -0,0 +1,36 @@
+import {cdn} from "./url";
+
+export const meetTheTeamData = [
+    {
+        person_img: `${cdn.url}/principal-investigator.png`,
+        person_name: "Dr. Burcu Kosar",
+        designation: "Principal Investigator"
+    },
+    {person_img: `${cdn.url}/co-Investigator.png`, person_name: "Dr. Jia Yue", designation: "Co-Investigator"},
+    {
+        person_img: `${cdn.url}/technical-consultant.png`,
+        person_name: "Dr. Jozsef Bor",
+        designation: "Technical Consultant"
+    },
+    {person_img: `${cdn.url}/ux-design.png`, person_name: "Sean Simmons", designation: "UX Design"},
+    {
+        person_img: `${cdn.url}/science-communication.png`,
+        person_name: "Rachel Lense",
+        designation: "Science Communication"
+    },
+    {
+        person_img: `${cdn.url}/ensemble-consultancy-1.png`,
+        person_name: "Oliver Gerland",
+        designation: "Website Development"
+    },
+    {
+        person_img: `${cdn.url}/ensemble-consultancy-2.png`,
+        person_name: "Aleigh Meredith",
+        designation: "Website Development"
+    },
+    {
+        person_img: `${cdn.url}/ensemble-consultancy-3.png`,
+        person_name: "Adam Karides",
+        designation: "Website Development"
+    }
+]
--- src/pages/Page/MeetTheTeam.jsx
+++ src/pages/Page/MeetTheTeam.jsx
@@ -1,25 +1,9 @@
 import "../../assets/scss/component/blog.scss";
-import { Button, Card, CardBody, CardColumns, CardGroup, CardImg, CardSubtitle, CardText, CardTitle, Col, Row } from "reactstrap";
-import principalInvestigator from '../../assets/images/principal-investigator.png';
-import coInvestigator from '../../assets/images/co-Investigator.png';
-import techConsultant from '../../assets/images/technical-consultant.png';
-import uxDesign from '../../assets/images/ux-design.png';
-import scienceCommunication from '../../assets/images/science-communication.png';
-import ensembleConsultancy1 from '../../assets/images/ensemble-consultancy-1.png';
-import ensembleConsultancy2 from '../../assets/images/ensemble-consultancy-2.png';
-import ensembleConsultancy3 from '../../assets/images/ensemble-consultancy-3.png';
+import {Card, CardBody, CardGroup, CardImg, CardSubtitle, CardText, CardTitle, Col, Row} from "reactstrap";
+import {meetTheTeamData} from "../../helpers/frontPage";
 
 const MeetTheTeam = () => {
-    const spritacular_team = [
-        { person_img: principalInvestigator, person_name: "Dr. Burcu Kosar", designation: "Principal Investigator" },
-        { person_img: coInvestigator, person_name: "Dr. Jia Yue", designation: "Co-Investigator" },
-        { person_img: techConsultant, person_name: "Dr. Jozsef Bor", designation: "Technical Consultant" },
-        { person_img: uxDesign, person_name: "Sean Simmons", designation: "UX Design" },
-        { person_img: scienceCommunication, person_name: "Rachel Lense", designation: "Science Communication" },
-        { person_img: ensembleConsultancy1, person_name: "Oliver Gerland", designation: "Website Development" },
-        { person_img: ensembleConsultancy2, person_name: "Aleigh Meredith", designation: "Website Development" },
-        { person_img: ensembleConsultancy3, person_name: "Adam Karides", designation: "Website Development" }
-    ]
+
 
     return (
         <div className="blog_page">
@@ -27,18 +11,21 @@ const MeetTheTeam = () => {
             <section className="blog-main">
                 <div className="container">
                     <h2 className="text-center mb-2">Meet The Team</h2>
-                    <p className="text-center mb-5">Spritacular project is developed and managed by a dedicated team!</p>
+                    <p className="text-center mb-5">Spritacular project is developed and managed by a dedicated
+                        team!</p>
                     <Row className="g-4">
                         <Col md={12}>
-                            <Card className="border-0 shadow-sm p-md-5 p-4">
+                            <Card className="border-0 shadow-sm p-md-5 p-4 team-card-box">
                                 <CardGroup className="mt-4 text-center mb-3">
-                                    {spritacular_team && spritacular_team.map((member, team_index) => (
+                                    {meetTheTeamData && meetTheTeamData.map((member, team_index) => (
                                         team_index < 5 &&
                                         (<Card className="align-items-center border-0 p-3" key={team_index}>
-                                            <CardImg className="w-75 h-100" alt={member.designation} src={member.person_img} top />
+                                            <CardImg className="w-75" alt={member.designation}
+                                                     src={member.person_img} top/>
                                             <CardBody>
-                                                <CardTitle tag="h4">{member.person_name}</CardTitle>
-                                                <CardSubtitle className="mb-2 text-muted" tag="h6">{member.designation}</CardSubtitle>
+                                                <CardTitle tag="h4" className="fs-5">{member.person_name}</CardTitle>
+                                                <CardSubtitle className="mb-2 text-muted"
+                                                              tag="h6">{member.designation}</CardSubtitle>
                                             </CardBody>
                                         </Card>)
                                     ))}
@@ -46,13 +33,16 @@ const MeetTheTeam = () => {
 
                                 {/* Ensemble Consultancies */}
                                 <CardGroup className="mt-5 text-center justify-content-around">
-                                    {spritacular_team && spritacular_team.map((member, team_index) => (
+                                    {meetTheTeamData && meetTheTeamData.map((member, team_index) => (
                                         team_index >= 5 &&
-                                        (<Card className="align-items-center py-3 border-0 h-100" style={{maxWidth:'25%'}}>
-                                            <CardImg alt="Ensemble Consultancy - 1" src={member.person_img} top className="w-50" />
+                                        (<Card className="align-items-center py-3 border-0 h-100"
+                                               style={{maxWidth: '25%'}} key={team_index}>
+                                            <CardImg alt="Ensemble Consultancy - 1" src={member.person_img} top
+                                                     className="w-50"/>
                                             <CardBody className="text-center">
-                                                <CardTitle tag="h4">{member.person_name}</CardTitle>
-                                                <CardSubtitle className="mb-2 text-muted" tag="h6">Ensemble Consultancy</CardSubtitle>
+                                                <CardTitle tag="h4" className="fs-5">{member.person_name}</CardTitle>
+                                                <CardSubtitle className="mb-2 text-muted" tag="h6">Ensemble
+                                                    Consultancy</CardSubtitle>
                                                 <CardText>{member.designation}</CardText>
                                             </CardBody>
                                         </Card>)
