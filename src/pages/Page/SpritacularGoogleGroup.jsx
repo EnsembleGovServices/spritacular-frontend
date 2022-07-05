29fe560a38a2585b1afa975999b427fc900fb058
inx-tapan <tapan.inx@gmail.com>
2022-07-05 13:20:21 +0530

    updated page components

--- src/pages/Page/SpritacularGoogleGroup.jsx
+++ src/pages/Page/SpritacularGoogleGroup.jsx
@@ -1,18 +1,14 @@
 import "../../assets/scss/component/blog.scss";
-import {lazy, Suspense} from "react";
-
 import {baseURL} from "../../helpers/url";
+import DynamicPageEditor from "../../components/Static/DynamicPageEditor";
 
-const DynamicPageEditor = lazy(() => import('../../components/Static/DynamicPageEditor'))
 
 const SpritacularGoogleGroup = () => {
     return (
-        <Suspense fallback={''}>
-            <DynamicPageEditor
-                title="Spritacular Google Group"
-                endpoint={baseURL.static_google_group}
-            />
-        </Suspense>
+        <DynamicPageEditor
+            title="Spritacular Google Group"
+            endpoint={baseURL.static_google_group}
+        />
     )
 }
 
